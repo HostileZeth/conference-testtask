1be012a411781f4e7f2bf78c4a0560323e491b4d
HostileZeth <sergeyataev@gmail.com>
2020-12-17 01:14:15 +0400

    initial commit

---
+++ src/test/java/com/sataev/conferencewebcrud/ConferenceWebCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sataev.conferencewebcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceWebCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
